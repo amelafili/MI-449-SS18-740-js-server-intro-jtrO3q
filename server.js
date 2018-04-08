5b838af19cc8031928e2f19a99c95762c1d360d2
amelafili <31477792+amelafili@users.noreply.github.com>
2018-04-08 18:05:35 -0400

    changed > to <

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var server = http.createServer(function (request, response) {
     )
   } else if (request.url === '/random-joke') {
     var num = Math.random()
-    if (num > 0.33) {
+    if (num < 0.33) {
       response.end('<h1>Funny jokes!</h1>' +
-       '<p>Knock, knock!</p> <p>Who\'s there?</p> <p>Opportunity!</p> ' +
-       '<p>That is impossible. Opportunity doesn’t come knocking twice!</p>')
-    } else if (num > 0.66) {
+      '<p>Knock, knock!</p> <p>Who\'s there?</p> <p>Opportunity!</p> ' +
+      '<p>That is impossible. Opportunity doesn’t come knocking twice!</p>')
+    } else if (num < 0.66) {
       response.end('<h1>Funny jokes!</h1>' +
       '<p>Knock, knock!</p> <p>Who\'s there?</p> <p>An extraterrestrial</p>' +
       '<p>Extraterrestrial who?</p> <p>What – how many extra-terrestrials do you know?</p>')
@@ -26,7 +26,7 @@ var server = http.createServer(function (request, response) {
   } else if (request.url === '/cuteness') {
     response.end(
       '<h1>Cute turtle!</h1>' +
-      '<img src="http://www.aws.org.au/wp-content/uploads/2017/04/iStock-619670754.jpg" alt="turtles"')
+      '<img src="http://www.aws.org.au/wp-content/uploads/2017/04/iStock-619670754.jpg" alt="turtles">')
   } else {
     response.end('<h1>Page Not Found</h1>' +
       '<p>The requested URL ' + request.url + ' was not found.</p>')
@@ -38,5 +38,5 @@ var server = http.createServer(function (request, response) {
 var port = process.env.PORT || 8080
 server.listen(port)
 
-    // Output a friendly message to the terminal
+// Output a friendly message to the terminal
 console.log('Server running at http://localhost:' + port + '/')

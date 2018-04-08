fd0a86b2cf83115cb31798f559897cf1811711c8
amelafili <31477792+amelafili@users.noreply.github.com>
2018-04-08 14:05:47 -0400

    fixed turtle picture

--- server.js
+++ server.js
@@ -17,18 +17,17 @@ var server = http.createServer(function (request, response) {
   } else if (request.url === '/cuteness') {
     response.end(
       '<h1>Cute turtle!</h1>' +
-      '<img src="http://www.aws.org.au/wp-content/uploads/2017/04/iStock-619670754.jpg" alt="turtles>')
+      '<img src="http://www.aws.org.au/wp-content/uploads/2017/04/iStock-619670754.jpg" alt="turtles"')
   } else {
     response.end('<h1>Page Not Found</h1>' +
-  '<p>The requested URL ' + request.url + ' was not found.</p>')
+      '<p>The requested URL ' + request.url + ' was not found.</p>')
   }
 })
 
-// Listen on port 8080, so that we can reach the app at
-// localhost:8080 and Heroku
+    // Listen on port 8080, so that we can reach the app at
+    // localhost:8080 and Heroku
 var port = process.env.PORT || 8080
-
 server.listen(port)
 
-// Output a friendly message to the terminal
+    // Output a friendly message to the terminal
 console.log('Server running at http://localhost:' + port + '/')
